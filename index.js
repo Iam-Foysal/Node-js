b017a6e0e911ad78ee3e726795b7dd4991ddb6d8
MD FOYSAL <146870309+Iam-Foysal@users.noreply.github.com>
2025-02-01 22:31:40 +0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let port = 6006;
 let myserver = http.createServer((req, res)=>{
 
     console.log("my server is running")
-    res.end("My Name is foysal")
+    res.end("My Name is md foysal ")
 })
 
 myserver.listen(port)
